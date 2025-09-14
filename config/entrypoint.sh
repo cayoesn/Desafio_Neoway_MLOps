4836b017f41e8c5b86accc5386338ee5f526d9b1
Cayo Eduardo da Silva Neves <cayoesn@gmail.com>
2025-09-14 20:52:32 -0300

    Refactor Dockerfile and docker-compose; update setup.py and entrypoint.sh for improved Airflow initialization and user setup

---
+++ config/entrypoint.sh
@@ -0,0 +1,16 @@
+set -e
+
+if [ ! -f "/opt/airflow/airflow.db" ]; then
+  echo ">>> Inicializando banco de dados do Airflow..."
+  airflow db init
+fi
+
+airflow users create \
+  --username "${_AIRFLOW_WWW_USER_USERNAME:-admin}" \
+  --firstname "${_AIRFLOW_WWW_USER_FIRSTNAME:-Airflow}" \
+  --lastname "${_AIRFLOW_WWW_USER_LASTNAME:-Admin}" \
+  --role Admin \
+  --email "${_AIRFLOW_WWW_USER_EMAIL:-admin@example.com}" \
+  --password "${_AIRFLOW_WWW_USER_PASSWORD:-admin}" || true
+
+exec "$@"
